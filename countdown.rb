b88676d1e6a76235fedd6f7281a5c9e73652b1eb
John Reyes <john.jreye@gmail.com>
2019-09-20 17:50:28 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,11 @@
 #write your code here
+
 def countdown(seconds_to_midnight)
   while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} Second(s)"
+    puts "#{seconds_to_midnight} SECOND(S)!"
     seconds_to_midnight -= 1
   end
-"Happy New Year"
+  "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(seconds_to_midnight)
