040d3daea9c18a3528a071c86b46c4c8d2bb9cd7
John Reyes <john.jreye@gmail.com>
2019-09-20 17:43:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,21 +1,17 @@
 #write your code here
-countdown = 10
-def countdown(n)
-loop do
-  while countdown < 10
-    counter -= 1
-    puts "#{countdown} seconds"
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} Second(s)"
+    seconds_to_midnight -= 1
 end
-  expect { countdown(10) }.to output ("Happy New Year")
-end
+"Happy New Year"
 end
 
-def countdown_with_sleep
-  countdown_with_sleep = 12
-while countdown_with_sleep > 0
-    puts "#{countdown_with_sleep} seconds"
-    countdown_with_sleep -= 1
-    sleep 5
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight}" > 0
+    sleep(1)
+    seconds_to_midnight -= 1
   end
   "Happy New Year"
 end
